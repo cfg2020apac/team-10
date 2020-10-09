0aed4ce8fe7bc4888faf11e2b6a3ed79d45f4547
seeziyang <44959961+seeziyang@users.noreply.github.com>
2020-10-09 21:57:18 +0800

    Testing signup

--- rn/pages/auth/Signup.js
+++ rn/pages/auth/Signup.js
@@ -9,23 +9,43 @@ import {
   KeyboardAvoidingView,
   Alert,
 } from 'react-native';
-import { Button, Input } from 'react-native-elements';
+import { Button, Input, CheckBox } from 'react-native-elements';
+import axios from 'axios';
 
 export default Signup = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [name, setName] = useState('');
+  const [organisationName, setOrganisationName] = useState('');
+  const [designation, setDesignation] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
 
   const signup = () => {
     if (!username || !password || !confirmPassword) {
-      Alert.alert('All fields must be entered!');
+      Alert.alert('Username and password must be entered!');
     }
 
     if (password !== confirmPassword) {
       Alert.alert('Password does not match!');
     }
 
-    // todo
+    axios
+      .post('https://codeitsuisse-mcspicy.herokuapp.com/createUser', {
+        officerID: username,
+        password,
+        officerType,
+        name,
+        organisationName,
+        designation,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+        Alert.alert('Error signing up user', err);
+      });
   };
 
   return (
@@ -44,6 +64,19 @@ export default Signup = () => {
           onChangeText={setConfirmPassword}
           secureTextEntry
         />
+        <Input label={'Name'} value={name} onChangeText={setName} />
+        <Input
+          label={'Organisation'}
+          value={organisationName}
+          onChangeText={setOrganisationName}
+        />
+        <Input
+          label={'Designation'}
+          value={designation}
+          onChangeText={setDesignation}
+        />
+
+        <CheckBox center title="Admin" checked={isAdmin} />
 
         <Button
           style={styles.buttonContainer}
