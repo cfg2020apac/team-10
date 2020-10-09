a5e7ea99054a48c2f202873302be20fc10ade36b
seeziyang <44959961+seeziyang@users.noreply.github.com>
2020-10-09 20:04:03 +0800

    Add login layout

--- rn/pages/auth/Login.js
+++ rn/pages/auth/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,26 +6,64 @@ import {
   View,
   Text,
   StatusBar,
+  KeyboardAvoidingView,
 } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons';
 
 import { UserContext } from '../../util/UserProvider';
 
 export default Login = () => {
   const { setAuthToken } = useContext(UserContext);
 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView>
-          <Text>TODO</Text>
+        <KeyboardAvoidingView style={styles.container}>
+          <Input
+            label={'Username'}
+            value={username}
+            onChangeText={setUsername}
+          />
+          <Input
+            label={'Password'}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <Button
+            style={styles.buttonContainer}
+            title="Login"
+            onPress={() => {}}
+          />
+          <Button
+            style={styles.buttonContainer}
+            title="Don't have an account? Signup"
+            onPress={() => {}}
+          />
+
           <Button
             title="fake login"
             onPress={() => setAuthToken('fake token')}
+            style={styles.buttonContainer}
           />
-        </ScrollView>
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    margin: 6,
+  },
+});
