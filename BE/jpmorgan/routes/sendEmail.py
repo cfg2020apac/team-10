cdc27f3853644bbf7ae1fdd5960eb0e8000424b5
tan0494d <chtan.2018@sis.smu.edu.sg>
2020-10-09 21:50:12 +0800

    Update sendEmail.py
    Updated credentials....

--- BE/jpmorgan/routes/sendEmail.py
+++ BE/jpmorgan/routes/sendEmail.py
@@ -12,14 +12,13 @@ from jpmorgan import app;
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = 'Constiislyf3#'
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
 
 @app.route("/acknowledgement/<string:emailAddress>",methods=["POST"])
 def send_email(emailAddress):
-    print("LASDA")
     if request.is_json:
         content = request.get_json()
     else:
