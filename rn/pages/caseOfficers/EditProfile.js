46da3e21efcfe4654deea1eb8060701c78760c53
seeziyang <44959961+seeziyang@users.noreply.github.com>
2020-10-09 23:19:07 +0800

    Add skeleton for all case officer pages

--- rn/pages/caseOfficers/EditProfile.js
+++ rn/pages/caseOfficers/EditProfile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,17 +6,53 @@ import {
   View,
   Text,
   StatusBar,
+  KeyboardAvoidingView,
+  Alert,
 } from 'react-native';
+import { Button, Input, CheckBox } from 'react-native-elements';
+import axios from 'axios';
 
 export default EditProfile = () => {
+  const [name, setName] = useState('');
+  const [organisationName, setOrganisationName] = useState('');
+  const [designation, setDesignation] = useState('');
+
+  const doneEditing = () => {
+    // todo
+  };
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView>
-          <Text>TODO</Text>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView>
+      <KeyboardAvoidingView style={styles.container}>
+        <Input label={'Name'} value={name} onChangeText={setName} />
+        <Input
+          label={'Organisation'}
+          value={organisationName}
+          onChangeText={setOrganisationName}
+        />
+        <Input
+          label={'Designation'}
+          value={designation}
+          onChangeText={setDesignation}
+        />
+
+        <Button
+          style={styles.buttonContainer}
+          title="Done"
+          onPress={doneEditing}
+        />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    margin: 6,
+  },
+});
--- rn/pages/caseOfficers/Profile.js
+++ rn/pages/caseOfficers/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -9,7 +9,27 @@ import {
 } from 'react-native';
 import { Button } from 'react-native-elements';
 
+/*
+{"officerID": "Danieltan123465", 
+"password": "holyCrap", 
+"officerType": "Admin", 
+"name": "Daniel Tan Tan",
+ "organisationName": "New hope", 
+ "designation":"MR" }
+*/
 export default Profile = ({ navigation }) => {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    if (profile == null) {
+      // todo get from user context
+      // setProfile();
+    } else {
+      const profId = props.profId;
+      // todo fetch from api
+    }
+  }, []);
+
   return (
     <SafeAreaView>
       <ScrollView>
@@ -19,6 +39,19 @@ export default Profile = ({ navigation }) => {
             title="Case Officers List"
             onPress={() => navigation.navigate('ViewAllCaseOfficers')}
           />
+          <Button
+            style={styles.buttonContainer}
+            title="Edit Profile"
+            onPress={() => navigation.navigate('EditProfile')}
+          />
+        </View>
+
+        <View>
+          <Text>Officer ID: {profile?.officerID}</Text>
+          <Text>Name: {profile?.name}</Text>
+          <Text>Organisation: {profile?.organisationName}</Text>
+          <Text>Designation: {profile?.desgination}</Text>
+          {/* todo: show admin or not */}
         </View>
       </ScrollView>
     </SafeAreaView>
