42d95ada19017a3abc8c984d1a7dc4e6979280ee
wyt-sonia <wyt.sonia@gmail.com>
2020-10-10 03:51:58 +0800

    link to backend

--- rn/pages/applicants/CreateNewApplicant.js
+++ rn/pages/applicants/CreateNewApplicant.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext }from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -7,13 +7,49 @@ import {
   Text,
   StatusBar,
   ActivityIndicator,
+  Alert,
 } from 'react-native';
+import axios from 'axios';
 
 import { Button, Input, Image} from 'react-native-elements';
 
 import { UserContext } from '../../util/UserProvider';
 
 export default CreateNewApplicant = () => {
+
+  const [applicantName, setApplicantName] = useState('');
+  const [applicantID, setApplicantID] = useState('');
+  const navigateToTodo = () => navigation.navigate('Todo');
+
+  const createNewApplicant = () => {
+    if (!applicantName) {
+      Alert.alert('Applicant Name be empty!');
+    }
+
+    if (!applicantID) {
+      Alert.alert('Applicant ID cannot be empty!');
+    }
+
+    axios
+      .post('https://codeitsuisse-mcspicy.herokuapp.com/addApplicant', {
+        name: applicantName,
+        applicantID: applicantID
+      })
+      .then((res) => {
+        const { data } = res;
+        setUserId(username);
+        setAuthToken(data.token);
+      })
+      .catch((err) => {
+        console.log('Error add applicant in', err);
+        if (applicantName.length != 0 && applicantName.length != 0) {
+          Alert.alert('Error on adding applicant');
+        }
+        
+      });
+  };
+
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -28,20 +64,24 @@ export default CreateNewApplicant = () => {
           />
 
           <Input  placeholder='Applicant name'
+                  value={applicantName}
                   inputContainerStyle={{ marginHorizontal: 10 }}
-                  style={{ marginTop: 100 }} />
+                  style={{ marginTop: 100 }}
+                  onChangeText={setApplicantName} />
           <Input  placeholder='Applicant ID'
-                  inputContainerStyle={{ marginHorizontal: 10 }}/>
+                  value={applicantID}
+                  inputContainerStyle={{ marginHorizontal: 10 }}
+                  onChangeText={setApplicantID}/>
           <Button
             title="Create New Applicant"
-            onPress={() => createNewApplicant('fake token')}
+            onPress={() => createNewApplicant()}
             style={{ marginTop: 80,
                      margin: 20 }}
           />
           
           <Button
             title="Cancel"
-            onPress={() => backToLastPage('fake token')}
+            onPress={navigateToTodo}
             style={{marginHorizontal: 20 }}
           />
         </ScrollView>
