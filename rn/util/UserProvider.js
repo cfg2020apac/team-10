5f982334c0a15765e42d6a6b8ba2cae410c9662e
seeziyang <44959961+seeziyang@users.noreply.github.com>
2020-10-10 01:31:57 +0800

    Persist user authentication

--- rn/util/UserProvider.js
+++ rn/util/UserProvider.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, createContext } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export const UserContext = createContext({});
 
@@ -6,13 +7,36 @@ function UserProvider(props) {
   const [userId, setUserId] = useState(null);
   const [authToken, setAuthToken] = useState(null);
 
-  useEffect(() => {
-    // todo
+  useEffect(async () => {
+    const storageUserId = await AsyncStorage.getItem('userId');
+    const storageAuthToken = await AsyncStorage.getItem('authToken');
+
+    setUserId(storageUserId);
+    setAuthToken(storageAuthToken);
   }, []);
 
   return (
     <UserContext.Provider
-      value={{ userId, authToken, setUserId, setAuthToken }}
+      value={{
+        userId,
+        authToken,
+        setUserId: (userId) => {
+          setUserId(userId);
+          if (userId) {
+            AsyncStorage.setItem('userId', userId);
+          } else {
+            AsyncStorage.removeItem('userId');
+          }
+        },
+        setAuthToken: (authToken) => {
+          setAuthToken(authToken);
+          if (authToken) {
+            AsyncStorage.setItem('authToken', authToken);
+          } else {
+            AsyncStorage.removeItem('authToken');
+          }
+        },
+      }}
     >
       {props.children}
     </UserContext.Provider>
