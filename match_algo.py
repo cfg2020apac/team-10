48e303cb9b9ac4c1da59cf3a9479fb6376497fc2
Richard Pung <31462291+savvyguru@users.noreply.github.com>
2020-10-10 11:03:10 +0800

    Update match_algo.py

--- match_algo.py
+++ match_algo.py
@@ -1,29 +1,37 @@
 from hungarian_algorithm import algorithm
 
+userA = ["male", "20-30", "chinese", "christian","day","smoker","sammy"]
+userB = ["male", "20-30", "malay","muslim","day","non-smoker","roy"]
+userC = ["female","30-40", "chinese", "christian","night", "smoker", "sara"]
+userD = ["female","40-50","chinese","christian","day","smoker","grace"]
+
 #user_attributes = [gender, age, race, religion,day/night,smoker,preference_weights,name]
 #bipartite match male and females?
 
-size = len(all_users)
-first_client_group = all_users[size//2:]
-second_client_group = all_users[:(size//2)+1]
+all_users = [userA,userC,userB,userD]
 
+half_size = len(all_users)//2
+first_client_group = all_users[half_size:]
+second_client_group = all_users[:half_size]
 
-def generate_top_preferences(preference_weights,second_client_group):
+def generate_top_preferences(user,second_client_group):
     #return ordered list of prefered matches
     pref_dict = {}
-    for user in second_client_group:
+    for user2 in second_client_group:
       score = 0
       for i in range(6):
-        score += preference_weights[i] * user[i]
-      pref_dict[second_client_group[7]] = score 
+        if user[i] == user2[i]:
+          score += 1
+      name = user2[6]
+      pref_dict[name] = score
     return pref_dict 
 
 def generate_bipartite_graph(first_client_group,second_client_group):
   G = {}
   for user in first_client_group:
-    G[user[7]] = generate_top_preferences(user[6],second_client_group)
+    G[user[6]] = generate_top_preferences(user,second_client_group)
   return G
 
-generate_bipartite_graph(first_client_group,second_client_group)
+G = generate_bipartite_graph(first_client_group,second_client_group)
 
-algorithm.find_matching(G, matching_type = 'min', return_type = 'list' )
+match = algorithm.find_matching(G, matching_type = 'max', return_type = 'list' )
