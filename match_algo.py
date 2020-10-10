b435eda0e1ea6f9e4e23b9e896cfe97f4469c438
Richard Pung <31462291+savvyguru@users.noreply.github.com>
2020-10-10 11:13:09 +0800

    Update match_algo.py

--- match_algo.py
+++ match_algo.py
@@ -1,9 +1,11 @@
 from hungarian_algorithm import algorithm
 
-userA = ["male", "20-30", "chinese", "christian","day","smoker","sammy"]
-userB = ["male", "20-30", "malay","muslim","day","non-smoker","roy"]
-userC = ["female","30-40", "chinese", "christian","night", "smoker", "sara"]
-userD = ["female","40-50","chinese","christian","day","smoker","grace"]
+#importance vectoe on a scale of 1-3
+
+userA = ["male", "20-30", "chinese", "christian","day","smoker","sammy",[3,2,2,1,1,2]]
+userB = ["male", "20-30", "malay","muslim","day","non-smoker","roy",[3,1,1,3,3,2]]
+userC = ["female","30-40", "chinese", "christian","night", "smoker", "sara",[2,3,3,3,2,3]]
+userD = ["female","40-50","chinese","christian","day","smoker","grace",[3,3,2,3,3,3]]
 
 #user_attributes = [gender, age, race, religion,day/night,smoker,preference_weights,name]
 #bipartite match male and females?
@@ -21,10 +23,10 @@ def generate_top_preferences(user,second_client_group):
       score = 0
       for i in range(6):
         if user[i] == user2[i]:
-          score += 1
+          score += 1 * user[7][i] * user2[7][i]
       name = user2[6]
       pref_dict[name] = score
-    return pref_dict 
+    return pref_dict
 
 def generate_bipartite_graph(first_client_group,second_client_group):
   G = {}
@@ -35,3 +37,5 @@ def generate_bipartite_graph(first_client_group,second_client_group):
 G = generate_bipartite_graph(first_client_group,second_client_group)
 
 match = algorithm.find_matching(G, matching_type = 'max', return_type = 'list' )
+
+print(match)
